251aac44f79ed3bf0dceb7b472514da56e903939
FlorianBreuker <code@servker.cc>
2025-01-26 22:31:27 +0100

    added a new version of grid layout (cols not centered but collapsed stuff now aligned

---
+++ src/lib/components/SetupItem.svelte
@@ -0,0 +1,33 @@
+<script>
+  import CollapsedSetupView from "./CollapsedSetupView.svelte";
+
+  export let id;
+  export let idle;
+  export let load;
+  export let cstate;
+  export let cpu;
+  export let mainboard;
+  export let os;
+  export let setup;
+</script>
+
+<div
+  tabindex="0"
+  role="button"
+  class="collapse grid grid-cols-subgrid col-span-7 font-black text-xs rounded-none"
+>
+  <div
+      class="collapse-title grid grid-cols-subgrid col-span-7 p-0 min-h-8"
+  >
+    <div>{id}</div>
+    <div>{idle}</div>
+    <div>{load}</div>
+    <div>{cstate}</div>
+    <div>{cpu}</div>
+    <div>{mainboard}</div>
+    <div>{os}</div>
+  </div>
+  <div class="collapse-content grid grid-cols-subgrid col-span-7 p-0">
+    <CollapsedSetupView {setup} />
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import SetupView from "$lib/components/SetupView.svelte";
+</script>
+
+<div class="flex justify-center mt-10">
+  <SetupView />
+</div>
