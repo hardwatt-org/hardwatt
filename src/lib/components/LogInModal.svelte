530a1e08e90b64038d77f44223365af3efa42553
FlorianBreuker <code@servker.cc>
2025-04-03 01:29:14 +0200

    customized loginmodal; added input validator class from daisyUI to Input elements; removed tooltip and changed the outer wrapper to relative; changed height of modalinput to a fixed not relative value, so the toggle has the same position on all devices

--- src/lib/components/LogInModal.svelte
+++ src/lib/components/LogInModal.svelte
@@ -2,22 +2,25 @@
     import Close from "$lib/components/Icons/Close.svelte";
     import GitHub from "$lib/components/Icons/GitHub.svelte";
 
-    const heading = "Log In"
-    const infoText = "Please sign in with on of the following services"
+    const heading = "Want to contribute your setup?"
+    const infoText = "Log in to share your system with the community."
 </script>
 
 <dialog id="LogIn" class="modal">
     <div class="modal-box w-10/12 2xl:h-1/4 xl:h-1/3 bg-base-300 items-center py-15 rounded-2xl">
-        <div class="absolute top-5 md:text-lg font-black">{heading}</div>
-        <div class="">{infoText}</div>
-        <GitHub/>
+        <div class="md:text-lg font-black">{heading}</div>
+        <div class="mt-2">{infoText}</div>
+        <button class="btn bg-black text-white border-black mt-5">
+            <GitHub h="h-5" w="h-5"/>
+            Login with GitHub
+        </button>
         <div class="modal-action">
             <form method="dialog">
                 <button class="absolute right-5 top-5">
                     <Close/>
                 </button>
-                <button class="btn btn-primary absolute right-5 bottom-5">Next
-                </button>
+                <!--<button class="btn btn-primary absolute right-5 bottom-5">Next
+                </button>-->
             </form>
         </div>
     </div>
