dd8824368042b5067a9ce6fafcf068abaf705ebe
EinGlasVollKakao <einglasvollkakao@vivaldi.net>
2025-04-21 20:14:40 +0200

    switched to ts and typing

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,10 @@
 import {pb, refreshUserState} from "$lib/api/pocketbase";
+import type { SetupRecord } from "$lib/api/types";
 
 export function load({}) {
     refreshUserState();
 
     return {
-        setups: pb.collection('setups').getFullList()
+        setups: pb.collection('setups').getFullList<SetupRecord>()
     };
 }
