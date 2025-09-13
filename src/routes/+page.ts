d9c9766f45f539f8fcece0f5644467c219143f1f
EinGlasVollKakao <einglasvollkakao@vivaldi.net>
2025-09-13 23:02:09 +0200

    refresh setups when user changes/adding a new one

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,8 +2,10 @@ import { pb } from "$lib/api";
 import { refreshUserState } from "$lib/auth.svelte";
 import type { SetupRecord } from "$lib/api/types";
 
-export function load({ fetch }) {
+export function load({ fetch, depends }) {
     refreshUserState();
+
+    depends("app:setups");
 
     return {
         setups: pb.collection('setups').getFullList<SetupRecord>({
