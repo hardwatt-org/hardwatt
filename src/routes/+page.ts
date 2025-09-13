a05ef470818cd6d6087fb640ca33d6cdbf58b5c8
EinGlasVollKakao <einglasvollkakao@vivaldi.net>
2025-09-13 23:02:09 +0200

    use sveltes `fetch` wich fixes `Loading https://pb.cacao.glass/api/... `window.fetch`. For best results, use the `fetch` that is passed to your `load` function: https://svelte.dev/docs/kit/load#making-fetch-requests` error in the browser console

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,11 +2,12 @@ import { pb } from "$lib/api";
 import { refreshUserState } from "$lib/auth.svelte";
 import type { SetupRecord } from "$lib/api/types";
 
-export function load({}) {
+export function load({ fetch }) {
     refreshUserState();
 
     return {
         setups: pb.collection('setups').getFullList<SetupRecord>({
+            fetch: fetch,
             sort: "-status,idle",
             requestKey: null
         })
