a2b34329e190139ca3d557d7cd76a1be7a586aae
FlorianBreuker <code@servker.cc>
2025-09-30 20:15:19 +0200

    used the skeleton as a long bar representing every setup; still using zebra, but the grid layout as in the text version is not used

--- src/lib/components/SetupView/Loading/SetupLoading.svelte
+++ src/lib/components/SetupView/Loading/SetupLoading.svelte
@@ -3,13 +3,7 @@
 </script>
 
 <div
-        class="{index % 2 === 0 ? 'bg-base-200' : 'bg-base-300'} grid grid-cols-subgrid col-span-8 p-0 min-h-8 h-10 items-center rounded-box"
+        class="{index % 2 === 0 ? 'bg-base-200' : 'bg-base-300'} grid col-span-8 p-0 min-h-8 h-10 items-center rounded-box"
 >
     <div class="ml-1 mr-4 skeleton h-5"></div>
-    <div class="mr-4 skeleton h-5"></div>
-    <div class="mr-4 skeleton h-5 hidden md:block"></div>
-    <div class="mr-4 skeleton h-5"></div>
-    <div class="mr-4 skeleton h-5 hidden md:block"></div>
-    <div class="mr-4 skeleton h-5 hidden xl:block"></div>
-    <div class="mr-4 skeleton h-5 hidden 2xl:block"></div>
 </div>
