7792e95ff962d76945cf217d07f31ec9e27103c3
FlorianBreuker <code@servker.cc>
2025-10-01 12:02:22 +0200

    went back to a text skeleton; but now only light rows are shown with a text skeleton, bc the skeleton is shown in bg-base which looks kind of shitty on the same background

--- src/lib/components/SetupView/Loading/SetupLoading.svelte
+++ src/lib/components/SetupView/Loading/SetupLoading.svelte
@@ -1,9 +1,19 @@
 <script>
     let {index} = $props();
+
+    const isEven = $derived(index % 2 === 0);
+    const bgClass = $derived(isEven ? 'bg-base-200' : 'bg-base-300');
+    const skeletonClass = $derived(isEven ? 'skeleton' : '');
 </script>
 
 <div
-        class="{index % 2 === 0 ? 'bg-base-200' : 'bg-base-300'} grid col-span-8 p-0 min-h-8 h-10 items-center rounded-box"
+        class="grid grid-cols-subgrid col-span-8 p-0 min-h-8 h-10 items-center rounded-box {bgClass}"
 >
-    <div class="ml-1 mr-4 skeleton h-5"></div>
+    <div class="ml-1 mr-4 h-5 {skeletonClass}"></div>
+    <div class="mr-4 h-5 {skeletonClass}"></div>
+    <div class="mr-4 h-5 hidden md:block {skeletonClass}"></div>
+    <div class="mr-4 h-5 {skeletonClass}"></div>
+    <div class="mr-4 h-5 hidden md:block {skeletonClass}"></div>
+    <div class="mr-4 h-5 hidden xl:block {skeletonClass}"></div>
+    <div class="mr-4 h-5 hidden 2xl:block {skeletonClass}"></div>
 </div>
